f7a330d5ae6aa0bc6367fd7811b04a8e6c4eb065
zhihuaiwen <zhihuaiwen@tencent.com>
2022-05-14 17:09:40 +0800

    update

--- spark-k8s-demo/src/main/java/com/spark/es/demo/SimpleWordCount.java
+++ spark-k8s-demo/src/main/java/com/spark/es/demo/SimpleWordCount.java
@@ -1,12 +1,18 @@
 package com.spark.es.demo;
 
-import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
+import scala.Tuple3;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author wenzhihuai
@@ -14,22 +20,91 @@ import java.util.List;
  */
 public class SimpleWordCount {
 
-    public static void main(String[] args) throws Exception {
+    public static final List<String> exampleApacheLogs = Arrays.asList(
+            "10.10.10.10 - \"FRED\" [18/Jan/2013:17:56:07 +1100] \"GET http://images.com/2013/Generic.jpg " +
+                    "HTTP/1.1\" 304 315 \"http://referall.com/\" \"Mozilla/4.0 (compatible; MSIE 7.0; " +
+                    "Windows NT 5.1; GTB7.4; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648; " +
+                    ".NET CLR 3.5.21022; .NET CLR 3.0.4506.2152; .NET CLR 1.0.3705; .NET CLR 1.1.4322; .NET CLR " +
+                    "3.5.30729; Release=ARP)\" \"UD-1\" - \"image/jpeg\" \"whatever\" 0.350 \"-\" - \"\" 265 923 934 \"\" " +
+                    "62.24.11.25 images.com 1358492167 - Whatup",
+            "10.10.10.10 - \"FRED\" [18/Jan/2013:18:02:37 +1100] \"GET http://images.com/2013/Generic.jpg " +
+                    "HTTP/1.1\" 304 306 \"http:/referall.com\" \"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; " +
+                    "GTB7.4; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648; .NET CLR " +
+                    "3.5.21022; .NET CLR 3.0.4506.2152; .NET CLR 1.0.3705; .NET CLR 1.1.4322; .NET CLR  " +
+                    "3.5.30729; Release=ARP)\" \"UD-1\" - \"image/jpeg\" \"whatever\" 0.352 \"-\" - \"\" 256 977 988 \"\" " +
+                    "0 73.23.2.15 images.com 1358492557 - Whatup");
 
-        // configure spark
-        SparkConf sparkConf = new SparkConf().setAppName("Print Elements of RDD")
-                .set("spark.executor.memory", "2g");
-        // start a spark context
-        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+    public static final Pattern apacheLogRegex = Pattern.compile(
+            "^([\\d.]+) (\\S+) (\\S+) \\[([\\w\\d:/]+\\s[+\\-]\\d{4})\\] \"(.+?)\" (\\d{3}) ([\\d\\-]+) \"([^\"]+)\" \"([^\"]+)\".*");
 
-        // sample collection
-        List<Integer> collection = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    /**
+     * Tracks the total query count and number of aggregate bytes for a particular group.
+     */
+    public static class Stats implements Serializable {
 
-        // parallelize the collection to two partitions
-        JavaRDD<Integer> rdd = sc.parallelize(collection);
+        private final int count;
+        private final int numBytes;
 
-        System.out.println("Number of partitions : " + rdd.getNumPartitions());
+        public Stats(int count, int numBytes) {
+            this.count = count;
+            this.numBytes = numBytes;
+        }
 
-        rdd.foreach((VoidFunction<Integer>) System.out::println);
+        public Stats merge(Stats other) {
+            return new Stats(count + other.count, numBytes + other.numBytes);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("bytes=%s\tn=%s", numBytes, count);
+        }
+    }
+
+    public static Tuple3<String, String, String> extractKey(String line) {
+        Matcher m = apacheLogRegex.matcher(line);
+        if (m.find()) {
+            String ip = m.group(1);
+            String user = m.group(3);
+            String query = m.group(5);
+            if (!user.equalsIgnoreCase("-")) {
+                return new Tuple3<>(ip, user, query);
+            }
+        }
+        return new Tuple3<>(null, null, null);
+    }
+
+    public static Stats extractStats(String line) {
+        Matcher m = apacheLogRegex.matcher(line);
+        if (m.find()) {
+            int bytes = Integer.parseInt(m.group(7));
+            return new Stats(1, bytes);
+        } else {
+            return new Stats(1, 0);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaLogQuery")
+                .master("local")
+                .getOrCreate();
+
+        try (JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext())) {
+            System.out.println("--------------start------------");
+            JavaRDD<String> dataSet = (args.length == 1) ? jsc.textFile(args[0]) : jsc.parallelize(exampleApacheLogs);
+
+            JavaPairRDD<Tuple3<String, String, String>, Stats> extracted =
+                    dataSet.mapToPair(s -> new Tuple2<>(extractKey(s), extractStats(s)));
+
+            JavaPairRDD<Tuple3<String, String, String>, Stats> counts = extracted.reduceByKey(Stats::merge);
+
+            List<Tuple2<Tuple3<String, String, String>, Stats>> output = counts.collect();
+            for (Tuple2<?, ?> t : output) {
+                System.out.println(t._1() + "\t" + t._2());
+            }
+            TimeUnit.SECONDS.sleep(1000);
+            spark.stop();
+        }
     }
 }
