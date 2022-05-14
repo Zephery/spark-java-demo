24c880ffcc7bc962be3495b1115c5c460292e036
zhihuaiwen <zhihuaiwen@tencent.com>
2022-05-14 17:36:56 +0800

    update

--- spark-k8s-demo/src/main/java/com/spark/es/demo/SimpleWordCount.java
+++ spark-k8s-demo/src/main/java/com/spark/es/demo/SimpleWordCount.java
@@ -84,14 +84,14 @@ public class SimpleWordCount {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        System.out.println("--------------start------------");
         SparkSession spark = SparkSession
                 .builder()
                 .appName("JavaLogQuery")
-                .master("local")
                 .getOrCreate();
 
         try (JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext())) {
-            System.out.println("--------------start------------");
+            System.out.println("--------------owjaofjeoifjwoifajwoifjow------------");
             JavaRDD<String> dataSet = (args.length == 1) ? jsc.textFile(args[0]) : jsc.parallelize(exampleApacheLogs);
 
             JavaPairRDD<Tuple3<String, String, String>, Stats> extracted =
