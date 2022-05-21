a29f0b6c92ba1b93f04a05c2728ffb0096275c82
zhihuaiwen <zhihuaiwen@tencent.com>
2022-05-21 18:56:48 +0800

    update

--- spark-es-demo/src/main/java/com/spark/es/demo/EsToMysqlDemo.java
+++ spark-es-demo/src/main/java/com/spark/es/demo/EsToMysqlDemo.java
@@ -4,8 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
 import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,11 +19,28 @@ import java.util.Map;
  * @since 2022/5/12 23:57
  */
 @Slf4j
-public class EsToMysqlDemo extends EsBaseConfig{
+public class EsToMysqlDemo extends EsBaseConfig {
     public static void main(String[] args) {
         SparkConf conf = getSparkConf();
         try (JavaSparkContext jsc = new JavaSparkContext(conf)) {
+            SparkSession sparkSession = SparkSession.builder()
+                    .config(conf)
+                    .getOrCreate();
             JavaRDD<Map<String, Object>> esRDD = JavaEsSpark.esRDD(jsc, "kibana_sample_data_ecommerce").values();
+            JavaRDD<Row> map = esRDD.map(v -> {
+                String currency = v.get("currency").toString();
+                String customerFullName = v.get("customer_full_name").toString();
+                String productsSku = v.getOrDefault("products", "").toString();
+
+                return RowFactory.create(currency, customerFullName, productsSku);
+            });
+            Dataset<Row> dataset = sparkSession.createDataFrame(map, StructType.fromDDL("currency string,customer_full_name string,products string"));
+            dataset.show(2);
+
+            Dataset<Row> count = dataset.select("currency").groupBy("currency").count();
+            count.show(2);
+
+
         }
     }
 }
